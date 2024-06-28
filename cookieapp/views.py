5c573bd2db2705989f50c1b098cd1f81376641d4
sekhar123-git <sekharchinna452@gmail.com>
2024-06-28 10:06:19 +0530

    9thcommit

---
+++ cookieapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def input(request):
+    return render(request,template_name='base.html')
+
+def add(request):
+    x=int(request.GET['t1'])
+    y=int(request.GET['t2'])
+    z=x+y
+    resp = HttpResponse('values submited successfully')
+    resp.set_cookie('z',z,max_age=100)
+    return resp
+
+def display(request):
+    if 'z' in request.COOKIES:
+        sum = request.COOKIES['z']
+        return HttpResponse("addition of two no:"+sum)
+    else:
+        return HttpResponse("pls enter values")
+
+
